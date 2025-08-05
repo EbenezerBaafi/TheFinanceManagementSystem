2ac90e21b2729081542481658125253468b2a8a0
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-05 02:28:01 +0000

    Add base Account class with AccountNumber, Balance, and transaction handling

---
+++ FinanceManagementSystem/FinanceManagementSystem/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementSystem
+{
+    public class Account
+    {
+        public string AccountNumber { get; init; }
+        public decimal Balance { get; protected set; }
+
+        public Account(string accountNumber, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public virtual void ApplyTransaction(Transaction transaction)
+        {
+            // Default implementation can be overridden by derived classes
+            Balance -= transaction.Amount;
+            Console.WriteLine($"Transaction has been applied: {transaction.Amount} to account {AccountNumber}. New balance: {Balance}");
+        }
+    }
+
+}
